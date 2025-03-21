31c139b66126ea1c62af76c1b830a866263cfe04
lxcong <83766787@qq.com>
2025-03-22 02:44:16 +0800

    fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mDQ/2AOdJOwV9qQDNjFZjEMVYXmJzZrYSruL+L8cdXw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
